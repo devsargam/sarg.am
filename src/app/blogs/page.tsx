78c558a9db8b3c4b95289266b32e2a8c5bafdca7
Sargam Poudel <sargampoudel100@gmail.com>
2025-05-12 12:25:25 +0530

    add blog page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,11 @@
+import { BlogNavigation } from '@/components/blog-navigation';
+import BlogSection from '@/components/blog-section';
+
+export default function Blogs() {
+  return (
+    <>
+      <BlogNavigation />
+      <BlogSection />
+    </>
+  );
+}
