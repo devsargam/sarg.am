f4dc4971dca81d606d17f5db79801bc32d4815ac
Sargam Poudel <sargampoudel100@gmail.com>
2025-05-12 12:33:49 +0530

    add metadata

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,5 +1,25 @@
 import { BlogNavigation } from '@/components/blog-navigation';
 import BlogSection from '@/components/blog-section';
+import { Metadata } from 'next';
+
+const title = 'Blog Posts';
+const description =
+  'Explore my thoughts, opinions, and insights on software development, lifestyle and midlife crisis.';
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: [{ url: '/og-image/blogs' }],
+  },
+  twitter: {
+    title,
+    description,
+    images: [{ url: '/og-image/blogs' }],
+  },
+};
 
 export default function Blogs() {
   return (
